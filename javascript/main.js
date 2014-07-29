3b9e8e01426ddd6fc29ccc94c153bca797ecd439
Piet van Dongen <piet@studio-donder.nl>
2014-07-29 12:39:12 +0200

    Now correctly parsing data

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,18 @@
-var dataFilePath = "data/example.csv";
+var dataFilePath = "/data/example.csv";
 
 console.clear();
 
-var data = d3.csv(dataFilePath, function (d) {
-    return {
-        Date: d3.time.format("%Y-%m-%d %H:%M:%S").parse(d.Date),
-        MaxCapacity: +d.MaxCapacity,
-        CurrentCapacity: +d.CurrentCapacity,
-        Charge: +d.CurrentCapacity / +d.MaxCapacity
-    };
-});
-
-console.log(data[0]);
+var data = d3.csv(
+    dataFilePath,
+    function (row) {
+        return {
+            Date: d3.time.format("%Y-%m-%d %H:%M:%S").parse(row.Date),
+            MaxCapacity: +row.MaxCapacity,
+            CurrentCapacity: +row.CurrentCapacity,
+            Charge: +row.CurrentCapacity / +row.MaxCapacity
+        }
+    },
+    function (error, rows) {
+        console.log(rows);
+    }
+);
